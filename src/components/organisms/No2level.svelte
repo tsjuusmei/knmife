929da2fe314557cc75aced99f35dce57232f77d2
Rick Groot <RickGroot@gmail.com>
2021-01-19 12:11:19 +0100

    added no2 maps

--- src/components/organisms/No2level.svelte
+++ src/components/organisms/No2level.svelte
@@ -28,10 +28,16 @@
   .map {
       width: 100%;
       height: 225px;
-      border-radius: 20px;
-      background: url('../images/map.placeholder.jpg') no-repeat;
-      background-size: cover;
-      background-position: center;
+      position: relative;
+  }
+
+  img {
+    display: block;
+    margin: 0 auto;
+    height: 100%;
+    width: 100%;
+    border-radius: 20px;
+    object-fit: cover;
   }
 
   sub {
@@ -51,37 +57,45 @@
         <div>
             <h5>Madrid</h5>
             <div class="map">
+              <img src="../images/no2maps/madrid.2019.jpg" alt="Madrid 2019">
             </div>
             <p>2019</p>
             <p>2020</p>
             <div class="map">
+              <img src="../images/no2maps/madrid.2020.jpg" alt="Madrid 2020">
             </div>
         </div>
         <div>
             <h5>Milaan</h5>
             <div class="map">
+              <img src="../images/no2maps/milan.2019.jpg" alt="Milaan 2019">
             </div>
             <p>2019</p>
             <p>2020</p>
             <div class="map">
+              <img src="../images/no2maps/milan.2020.jpg" alt="Milaan 2020">
             </div>
         </div>
         <div>
             <h5>Lyon</h5>
             <div class="map">
+              <img src="../images/no2maps/lyon.2019.jpg" alt="Lyon 2019">
             </div>
             <p>2019</p>
             <p>2020</p>
             <div class="map">
+              <img src="../images/no2maps/lyon.2020.jpg" alt="Lyon 2020">
             </div>
         </div>
         <div>
             <h5>Liverpool</h5>
             <div class="map">
+              <img src="../images/no2maps/liverpool.2019.jpg" alt="Liverpool 2019">
             </div>
             <p>2019</p>
             <p>2020</p>
             <div class="map">
+              <img src="../images/no2maps/liverpool.2020.jpg" alt="Liverpool 2020">
             </div>
         </div>
     </div>
