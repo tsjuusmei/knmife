84e7e1e4ef51d85499e35ca0cf7f7db0cc0cb235
tsjuusmei <guusmay2@hotmail.com>
2021-01-21 09:09:39 +0100

    added pathing

--- src/components/organisms/Restrictions.svelte
+++ src/components/organisms/Restrictions.svelte
@@ -14,7 +14,7 @@
 
     &::before,
     &::after {
-      content: url('../images/shape-rounded.svg');
+      content: url('/assets/images/shape-rounded.svg');
       margin: -5px 0;
       display: block;
 
