35e35216bcd0f3c7c50fc9d0fea273cd0ae56511
Rick Groot <rickgroot41@gmail.com>
2021-01-21 09:44:24 +0100

    start of chart

--- src/components/molecules/ChartExample.svelte
+++ src/components/molecules/ChartExample.svelte
@@ -1,116 +1,41 @@
 <script>
-  import { scaleLinear } from 'd3-scale';
-  import points from '../../../public/data/graph';
-
-  const yTicks = [0, 2, 4, 6, 8];
-  const xTicks = [1980, 1990, 2000, 2010];
-  const padding = { top: 20, right: 15, bottom: 20, left: 25 };
-
-  let width = 500;
-  let height = 200;
-
-  $: xScale = scaleLinear()
-    .domain([minX, maxX])
-    .range([padding.left, width - padding.right]);
-
-  $: yScale = scaleLinear()
-    .domain([Math.min.apply(null, yTicks), Math.max.apply(null, yTicks)])
-    .range([height - padding.bottom, padding.top]);
-
-  $: minX = points[0].x;
-  $: maxX = points[points.length - 1].x;
-  $: path = `M${points.map((p) => `${xScale(p.x)},${yScale(p.y)}`).join('L')}`;
-  $: area = `${path}L${xScale(maxX)},${yScale(0)}L${xScale(minX)},${yScale(
-    0
-  )}Z`;
-
-  function formatMobile(tick) {
-    return "'" + tick.toString().slice(-2);
-  }
+  
 </script>
 
-<h2>Arctic sea ice minimum</h2>
-
-<div class="chart" bind:clientWidth={width} bind:clientHeight={height}>
-  <svg>
-    <!-- y axis -->
-    <g class="axis y-axis" transform="translate(0, {padding.top})">
-      {#each yTicks as tick}
-        <g
-          class="tick tick-{tick}"
-          transform="translate(0, {yScale(tick) - padding.bottom})">
-          <line x2="100%" />
-          <text y="-4">{tick} {tick === 8 ? ' million sq km' : ''}</text>
-        </g>
-      {/each}
-    </g>
-
-    <!-- x axis -->
-    <g class="axis x-axis">
-      {#each xTicks as tick}
-        <g
-          class="tick tick-{tick}"
-          transform="translate({xScale(tick)},{height})">
-          <line y1="-{height}" y2="-{padding.bottom}" x1="0" x2="0" />
-          <text y="-2">{width > 380 ? tick : formatMobile(tick)}</text>
-        </g>
-      {/each}
-    </g>
-
-    <!-- data -->
-    <!-- <path class="path-area" d={area} /> -->
-    <path class="path-line" d={path} />
-  </svg>
-</div>
+<section>
+  <div class="container">
+    <ul>
+      <li>Madrid</li>
+      <li>Milaan</li>
+      <li>Lyon</li>
+      <li>Liverpool</li>
+    </ul>
+   <div class="group">
+      <img src="/images/charts/madridbar.svg" alt="">
+      <img src="/images/charts/madridline.svg" alt="">
+    </div>
+  </div>
+</section>
 
 <style>
-  .chart,
-  h2 {
-    width: 100%;
-    max-width: 500px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-
-  svg {
+  .group {
+    display: block;
     position: relative;
     width: 100%;
-    height: 200px;
-    overflow: visible;
+    margin: 0 auto;
   }
 
-  .tick {
-    font-size: 0.725em;
-    font-weight: 200;
+  .group img {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    top: 0;
+    right: 0;
   }
 
-  .tick line {
-    stroke: #aaa;
-    stroke-dasharray: 2;
-  }
-
-  .tick text {
-    fill: #666;
-    text-anchor: start;
-  }
-
-  .tick.tick-0 line {
-    stroke-dasharray: 0;
-  }
-
-  .x-axis .tick text {
-    text-anchor: middle;
-  }
-
-  .path-line {
-    fill: none;
-    stroke: rgb(0, 100, 100);
-    stroke-linejoin: round;
-    stroke-linecap: round;
-    stroke-width: 2;
-  }
-
-  .path-area {
-    fill: rgba(0, 100, 100, 0.2);
+  .group img:nth-child(2) {
+    padding-left: 4.5%;
+    padding-right: 10%;
+    padding-bottom: 5%;
   }
 </style>
